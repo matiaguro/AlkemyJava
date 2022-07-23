64a6c24f2fb83ce645c26b21d399a120efb1486e
Romero Matias Agustin <marogu_romero@hotmail.com>
2022-07-23 12:36:22 -0300

    Challenger inicio

---
+++ src/main/java/com/example/ChallengerJava/ChallengerJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ChallengerJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengerJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengerJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ChallengerJava/ChallengerJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ChallengerJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengerJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
